d166f2921d893335b240c51a8d66fcf60fadaf31
Mr-robot <hackingtest017@gmail.com>
2024-10-31 23:02:50 +0300

    add table synchronization

--- sequelize/app.js
+++ sequelize/app.js
@@ -5,6 +5,8 @@ import User from './models/user.js';
 const main = async () => {
     try {
         await db.authenticate();
+        await db.sync();
+        customLog.success('Connection has been established successfully.');
     } catch (error) {
         customLog.error('Unable to connect to the database:', error);
     }
