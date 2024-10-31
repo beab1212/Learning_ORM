f64d1b79fd5a11477ee1b65cd35ce40299723d1c
Mr-robot <hackingtest017@gmail.com>
2024-10-31 23:01:40 +0300

    implement test

--- sequelize/app.js
+++ sequelize/app.js
@@ -1,1 +1,13 @@
-console.log('code csompleted')
+import customLog from './utils/custom_log.js';
+import db from './db/index.js';
+import User from './models/user.js';
+
+const main = async () => {
+    try {
+        await db.authenticate();
+    } catch (error) {
+        customLog.error('Unable to connect to the database:', error);
+    }
+};
+
+main();
