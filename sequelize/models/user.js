d8e57dbf9c3c2a13bb281c254ce416a53d04afed
Mr-robot <hackingtest017@gmail.com>
2024-10-31 22:59:35 +0300

    create user model

--- sequelize/models/user.js
+++ sequelize/models/user.js
@@ -1,1 +1,15 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../db/index.js';
 
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUIDV,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    firstName: {
+        type: DataTypes.STRING(20),
+    },
+});
+
+export default User;
